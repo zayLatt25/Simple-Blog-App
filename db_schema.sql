bfb98eeee3e8adaa649bd05d4410bab33530176d
Mac <zaypainglatt04@gmail.com>
2024-07-10 23:11:53 +0800

    Reader home page (#2)
    * removed comments in users.js
    
    * added reader home page
    
    * changed db schema to fit for articles
    
    * added both reader and writer in schema
    
    * changed main page UI
    
    * removed extra comments in db
    
    * made reader dynamic
    
    * changed db schema to shorten name
    
    * added dynamic articles for new and featured
    
    * added dynamic blogs
    
    * removed article.id in query
    
    * fixed errorc duplicate blogs
    
    * removed check for pull request
    
    * removed comments

--- db_schema.sql
+++ db_schema.sql
@@ -1,34 +1,51 @@
-
--- This makes sure that foreign_key constraints are observed and that errors will be thrown for violations
 PRAGMA foreign_keys=ON;
 
 BEGIN TRANSACTION;
 
--- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
+-- CREATE TABLE IF NOT EXISTS email_accounts (
+--     email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     email_address TEXT NOT NULL,
+--     reader_id  INT, --the user that the email account belongs to
+--     FOREIGN KEY (writer_id) REFERENCES users(writer_id)
+-- );
 
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS readers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS email_accounts (
-    email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
-    user_id  INT, --the user that the email account belongs to
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS writers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    blogTitle TEXT NOT NULL,
+    blogSubtitle TEXT NOT NULL
 );
 
--- Insert default data (if necessary here)
+CREATE TABLE IF NOT EXISTS articles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    writerID INT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (writerID) REFERENCES writers(id)
+);
 
--- Set up three users
-INSERT INTO users ('user_name') VALUES ('Simon Star');
-INSERT INTO users ('user_name') VALUES ('Dianne Dean');
-INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
+INSERT INTO readers ('name') VALUES ('Simon Star');
+INSERT INTO readers ('name') VALUES ('Dianne Dean');
 
--- Give Simon two email addresses and Diane one, but Harry has none
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
+INSERT INTO writers ('name', 'blogTitle', 'blogSubtitle') VALUES ('Starry Traveler', 'Traveling the world', 'One country at a time');
+INSERT INTO writers ('name','blogTitle', 'blogSubtitle') VALUES ('Quantum Quill', 'Writing about the universe', 'One article at a time');
+
+-- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('simon@gmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('simon@hotmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('dianne@yahoo.co.uk', 2); 
+
+INSERT INTO articles ('title', 'content', 'writerID') VALUES ('My first article', 'This is the content of my first article', 1); 
+INSERT INTO articles ('title', 'content', 'writerID') VALUES ('My second article', 'I am writing an article for the second time', 1); 
+INSERT INTO articles ('title', 'content', 'writerID') VALUES ('My third article', 'Okay this is the third time', 1); 
+INSERT INTO articles ('title', 'content', 'writerID') VALUES ('I am a new writer', 'New writer first time', 2); 
+INSERT INTO articles ('title', 'content', 'writerID') VALUES ('This is defo different', 'Yeaa new second time', 2); 
 
 COMMIT;
 
---
+++ routes/reader-home.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  queryNewArticles =
+    "SELECT articles.title, articles.content, articles.createdAt, articles.updatedAt, writers.name AS writerName FROM articles JOIN writers ON articles.writerID = writers.id ORDER BY articles.createdAt DESC LIMIT 3";
+
+  queryFeaturedArticles =
+    "SELECT articles.title, articles.content, articles.createdAt, articles.updatedAt, writers.name AS writerName FROM articles JOIN writers ON articles.writerID = writers.id";
+
+  queryBlogs = "SELECT name, blogTitle, blogSubtitle FROM writers";
+
+  db.all(queryNewArticles, function (err, newArticles) {
+    if (err) {
+      next(err);
+    } else {
+      db.all(queryFeaturedArticles, function (err, featuredArticles) {
+        if (err) {
+          next(err);
+        } else {
+          db.all(queryBlogs, function (err, blogs) {
+            if (err) {
+              next(err);
+            } else {
+              res.render("reader-home.ejs", {
+                newArticles,
+                featuredArticles,
+                blogs,
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
