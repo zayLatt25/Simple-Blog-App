03d6c418502126fabbcb5772a64188650b2ecb2f
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-10 23:09:57 +0800

    removed check for pull request

--- routes/reader-home.js
+++ routes/reader-home.js
@@ -35,38 +35,4 @@ router.get("/", (req, res, next) => {
   });
 });
 
-router.get("/check", (req, res, next) => {
-  queryNewArticles =
-    "SELECT articles.title, articles.content, articles.createdAt, articles.updatedAt, writers.name AS writerName FROM articles JOIN writers ON articles.writerID = writers.id ORDER BY articles.createdAt DESC LIMIT 3";
-
-  queryFeaturedArticles =
-    "SELECT articles.title, articles.content, articles.createdAt, articles.updatedAt, writers.name AS writerName FROM articles JOIN writers ON articles.writerID = writers.id";
-
-  queryBlogs = "SELECT name, blogTitle, blogSubtitle FROM writers";
-
-  db.all(queryNewArticles, function (err, newArticles) {
-    if (err) {
-      next(err);
-    } else {
-      db.all(queryFeaturedArticles, function (err, featuredArticles) {
-        if (err) {
-          next(err);
-        } else {
-          db.all(queryBlogs, function (err, blogs) {
-            if (err) {
-              next(err);
-            } else {
-              res.json({
-                newArticles,
-                featuredArticles,
-                blogs,
-              });
-            }
-          });
-        }
-      });
-    }
-  });
-});
-
 module.exports = router;
