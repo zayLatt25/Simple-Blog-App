a6298f71471b50c400195a8751e342d0d7bbf23f
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-10 18:44:32 +0800

    changed db schema to shorten name

--- db_schema.sql
+++ db_schema.sql
@@ -5,13 +5,13 @@ PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS readers (
-    reader_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    reader_name TEXT NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS writers (
-    writer_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    writer_name TEXT NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
 );
 
 -- CREATE TABLE IF NOT EXISTS email_accounts (
@@ -22,26 +22,26 @@ CREATE TABLE IF NOT EXISTS writers (
 -- );
 
 CREATE TABLE IF NOT EXISTS articles (
-    article_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    article_title TEXT NOT NULL,
-    article_content TEXT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
     writer_id INT, 
-    FOREIGN KEY (writer_id) REFERENCES writers(writer_id)
+    FOREIGN KEY (writer_id) REFERENCES writers(id)
 );
 
 -- Set up three users
-INSERT INTO readers ('reader_name') VALUES ('Simon Star');
-INSERT INTO readers ('reader_name') VALUES ('Dianne Dean');
+INSERT INTO readers ('name') VALUES ('Simon Star');
+INSERT INTO readers ('name') VALUES ('Dianne Dean');
 
-INSERT INTO writers ('writer_name') VALUES ('Starry Traveler');
-INSERT INTO writers ('writer_name') VALUES ('Quantum Quill');
+INSERT INTO writers ('name') VALUES ('Starry Traveler');
+INSERT INTO writers ('name') VALUES ('Quantum Quill');
 
 -- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('simon@gmail.com', 1); 
 -- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('simon@hotmail.com', 1); 
 -- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('dianne@yahoo.co.uk', 2); 
 
-INSERT INTO articles ('article_title', 'article_content', 'writer_id') VALUES ('My first article', 'This is the content of my first article', 1); 
-INSERT INTO articles ('article_title', 'article_content', 'writer_id') VALUES ('My second article', 'I am writing an article for the second time', 1); 
+INSERT INTO articles ('title', 'content', 'writer_id') VALUES ('My first article', 'This is the content of my first article', 1); 
+INSERT INTO articles ('title', 'content', 'writer_id') VALUES ('My second article', 'I am writing an article for the second time', 1); 
 
 COMMIT;
 
