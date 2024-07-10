2f30a050135c55f873c046bd265b9fc899afcf3e
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-10 18:44:17 +0800

    made reader dynamic

--- routes/reader.js
+++ routes/reader.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/reader-home", (req, res) => {
-  res.render("reader-home.ejs");
+router.get("/reader-home", (req, res, next) => {
+  queryArticles = "SELECT * FROM articles";
+  //   queryBlogs = "SELECT * FROM blogs";
+
+  db.all(queryArticles, function (err, rows) {
+    if (err) {
+      next(err);
+    } else {
+      res.render("reader-home.ejs", { articles: rows });
+    }
+  });
 });
 
 module.exports = router;
