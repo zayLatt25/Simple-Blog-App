0ba918ba09ecf0251110f19d436ba8b6ee4c871c
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-10 18:15:17 +0800

    added both reader and writer in schema

--- db_schema.sql
+++ db_schema.sql
@@ -6,40 +6,44 @@ BEGIN TRANSACTION;
 
 -- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
 
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS readers (
+    reader_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    reader_name TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS email_accounts (
-    email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
-    user_id  INT, --the user that the email account belongs to
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS writers (
+    writer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    writer_name TEXT NOT NULL
 );
+
+-- CREATE TABLE IF NOT EXISTS email_accounts (
+--     email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     email_address TEXT NOT NULL,
+--     reader_id  INT, --the user that the email account belongs to
+--     FOREIGN KEY (writer_id) REFERENCES users(writer_id)
+-- );
 
 CREATE TABLE IF NOT EXISTS articles (
     article_id INTEGER PRIMARY KEY AUTOINCREMENT,
     article_title TEXT NOT NULL,
     article_content TEXT NOT NULL,
-    user_id INT, 
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    writer_id INT, 
+    FOREIGN KEY (writer_id) REFERENCES writers(writer_id)
 );
 
--- Insert default data (if necessary here)
-
 -- Set up three users
-INSERT INTO users ('user_name') VALUES ('Simon Star');
-INSERT INTO users ('user_name') VALUES ('Dianne Dean');
-INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
+INSERT INTO readers ('reader_name') VALUES ('Simon Star');
+INSERT INTO readers ('reader_name') VALUES ('Dianne Dean');
 
--- Give Simon two email addresses and Diane one, but Harry has none
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
+INSERT INTO writers ('writer_name') VALUES ('Starry Traveler');
+INSERT INTO writers ('writer_name') VALUES ('Quantum Quill');
 
-INSERT INTO articles ('article_title', 'article_content', 'user_id') VALUES ('My first article', 'This is the content of my first article', 1); 
-INSERT INTO articles ('article_title', 'article_content', 'user_id') VALUES ('My second article', 'This is the content of my second article', 1); 
+-- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('simon@gmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('simon@hotmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'writer_id') VALUES ('dianne@yahoo.co.uk', 2); 
+
+INSERT INTO articles ('article_title', 'article_content', 'writer_id') VALUES ('My first article', 'This is the content of my first article', 1); 
+INSERT INTO articles ('article_title', 'article_content', 'writer_id') VALUES ('My second article', 'I am writing an article for the second time', 1); 
 
 COMMIT;
 
