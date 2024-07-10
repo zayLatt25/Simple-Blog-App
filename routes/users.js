4bbe986ab1602ad90dacbbfaffbc38f1443ae040
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-10 17:32:59 +0800

    removed comments in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,3 @@
-/**
- * users.js
- * These are example routes for user management
- * This shows how to correctly structure your routes for the project
- * and the suggested pattern for retrieving data by executing queries
- *
- * NB. it's better NOT to use arrow functions for callbacks with the SQLite library
- *
- */
-
 const express = require("express");
 const router = express.Router();
 
