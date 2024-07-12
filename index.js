19800c6997cb220e98b21c69ce320ac9cdaf46b0
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-12 15:32:50 +0800

    created logout route and moved checkLogin to helper functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const session = require("express-session");
+const { checkLogin } = require("./utils/helper-functions");
+
 const app = express();
 const port = 3000;
 var bodyParser = require("body-parser");
@@ -28,45 +30,23 @@ app.use(
   })
 );
 
-app.post("/logout", (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return res.status(500).send("Logout failed!");
-    }
-    res.clearCookie("connect.sid");
-    res.status(200).send("Logged out successfully");
-  });
-});
-
-// Middleware function to check login
-function checkLogin(req, res, next) {
-  if (req.session.authenticated) {
-    next();
-  } else {
-    req.session.redirectTo = req.originalUrl;
-    res.redirect("/login");
-  }
-}
-
+// Routes that doesnt need login
 const mainPageRoutes = require("./routes/main-page");
 app.use("/", mainPageRoutes);
-
 const loginRoutes = require("./routes/login");
 app.use("/login", loginRoutes);
-
+const logoutRoutes = require("./routes/logout");
+app.use("/logout", logoutRoutes);
 const signUpRoutes = require("./routes/signup");
 app.use("/signup", signUpRoutes);
-
 const readerRoutes = require("./routes/reader");
 app.use("/reader", readerRoutes);
-
 const articleRoutes = require("./routes/article");
 app.use("/article", articleRoutes);
 
 //Routes that require login are protected by middleware
 const authorRoutes = require("./routes/author");
 app.use("/author", checkLogin, authorRoutes);
-
 const usersRoutes = require("./routes/users");
 app.use("/users", checkLogin, usersRoutes);
 
--- utils/helper-functions.js
+++ utils/helper-functions.js
@@ -9,4 +9,14 @@ function cutText(text, wordLimit) {
   return text;
 }
 
-module.exports = { cutText };
+// Middleware function to check login
+function checkLogin(req, res, next) {
+  if (req.session.authenticated) {
+    next();
+  } else {
+    req.session.redirectTo = req.originalUrl;
+    res.redirect("/login");
+  }
+}
+
+module.exports = { cutText, checkLogin };
