fbb08612fe00e86f45656c9356b017c8f6a0ddd7
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-12 15:41:56 +0800

    fixed logout route bug

--- routes/logout.js
+++ routes/logout.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/logout", (req, res) => {
+router.post("/", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       return res.status(500).send("Logout failed!");
