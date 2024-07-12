1ee9cbd6a0dd24e49ae6145794034b9e8496c3bf
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-12 16:14:52 +0800

    changed session storing email to storing id

--- routes/author.js
+++ routes/author.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { cutText } = require("../utils/helper-functions");
 
 router.get("/home", (req, res) => {
-  const email = req.session.user.email;
+  const authorId = req.session.user.id;
 
   // Left join is used so that if the author has no articles,
   // the author's info will still be fetched'
@@ -15,6 +15,7 @@ router.get("/home", (req, res) => {
             au.blogSubtitle,
             ar.title,
             ar.content,
+            ar.likes,
             ar.published,
             ar.createdAt,
             ar.updatedAt
@@ -23,9 +24,9 @@ router.get("/home", (req, res) => {
         LEFT JOIN 
             articles ar ON au.id = ar.authorID
         WHERE 
-            au.email = ?;`;
+            au.id = ?;`;
 
-  db.all(query, [email], (err, data) => {
+  db.all(query, [authorId], (err, data) => {
     if (err) {
       res.sendStatus(500);
       return;
@@ -37,6 +38,15 @@ router.get("/home", (req, res) => {
       cutText,
     });
   });
+});
+
+router.post("/new-article", (req, res) => {
+  const { title, content, published } = req.body;
+  const authorID = req.session.user.id;
+
+  const query = `
+        INSERT INTO articles (title, content, authorID)
+        VALUES (?, ?, ?);`;
 });
 
 module.exports = router;
