9fec7f46b745c849253bade53ae56851af96a7c6
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-12 15:42:11 +0800

    added consistency to sql queries

--- routes/article.js
+++ routes/article.js
@@ -4,10 +4,10 @@ const router = express.Router();
 router.get("/:articleId", (req, res, next) => {
   const articleId = req.params.articleId;
 
-  const queryArticle = ` SELECT articles.*, authors.name AS authorName
-    FROM articles
-    JOIN authors ON articles.authorID = authors.id
-    WHERE articles.id = ?`;
+  const queryArticle = ` SELECT ar.*, au.name AS authorName
+    FROM articles ar
+    JOIN authors au ON ar.authorID = au.id
+    WHERE au.id = ?`;
 
   db.get(queryArticle, [articleId], (err, article) => {
     if (err) {
